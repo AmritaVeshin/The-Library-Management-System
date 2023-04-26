a82abf749280c7e6590458fd35e2f54ef5ab5cb5
Atharva Vetal <vetalatharva13@gmail.com>
2023-04-26 10:18:46 +0530

    Java Project

---
+++ Library_Management_Systmem/src/main/java/com/mycompany/library_management_systmem/Library_Management_Systmem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.library_management_systmem;
+
+/**
+ *
+ * @author vetal
+ */
+public class Library_Management_Systmem {
+
+    public static void main(String[] args) {
+        
+        //code of application
+    }
+}
